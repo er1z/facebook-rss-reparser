44a3a085f8d1985c760a3333f18869f30dc17333
Paweł Sołtysiak <soltys@uznam.org>
2014-09-04 23:35:31 +0200

    code reformat

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ var url = require('url');
 var express = require('express'),
     app = express();
 
-
-
 var port = require('./package.json').options.port;
 
 app.locals.pretty = true;
@@ -18,66 +16,66 @@ app.set('view engine', 'jade');
 
 app.use(errorHandler());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.route('/')
-.get(function  (req,res) {
-    res.render('index');
-})
-.post(function  (req,res) {
-    
-    var pageUrl = req.body.pageUrl;
-    request({
-        url: pageUrl,
-        headers:
-        {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:31.0) Gecko/20100101 Firefox/31.0'
-        },
-    },function  (error,response,body) {
-        var re = /page_id=(\d+)/; 
-        var match = re.exec(body);
-        var pageId = match[1];
-        res.redirect('/reparse?id='+pageId);
+    .get(function(req, res) {
+        res.render('index');
+    })
+    .post(function(req, res) {
+      
+        var pageUrl = req.body.pageUrl;
+        request({
+            url: pageUrl,
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:31.0) Gecko/20100101 Firefox/31.0'
+            },
+        }, function(error, response, body) {
+            var re = /page_id=(\d+)/;
+            var match = re.exec(body);
+            var pageId = match[1];
+            res.redirect('/reparse?id=' + pageId);
+        });
     });
-});
 
-app.route('/reparse').get(function(req,res){
+app.route('/reparse').get(function(req, res) {
 
 
-    if(!req.query.id){
+    if (!req.query.id) {
         res.status(404);
         return;
     }
 
     var source = request({
-        url: 'http://www.facebook.com/feeds/page.php?format=atom10&id='+req.query.id,
-        headers:
-        {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:31.0) Gecko/20100101 Firefox/31.0'
-        }
-    }, function(error, response, body){
-        
-        if(response.statusCode!==200){
-            res.status(response.statusCode);
-            return
-        }
+            url: 'http://www.facebook.com/feeds/page.php?format=atom10&id=' + req.query.id,
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:31.0) Gecko/20100101 Firefox/31.0'
+            }
+        },function(error, response, body) {
+
+            if (response.statusCode !== 200) {
+                res.status(response.statusCode);
+                return
+            }
             var $ = cheerio.load(body, {
                 xmlMode: true
             });
-            
+
             var contents = $('content');
-            
-            contents.each(function(i){
+
+            contents.each(function(i) {
                 var content = contents.eq(i).html();
-                
+
                 content = content.replace(/<!\[CDATA\[([^\]]+)\]\]>/ig, "$1");
-                
+
                 var sub = cheerio.load(content);
                 var subs = sub('a');
-                
-                subs.each(function(i,v){
+
+                subs.each(function(i, v) {
                     var href = subs.eq(i).attr('href');
-                    
+
                     var url_parts = url.parse(href, true);
                     var query = url_parts.query;
 
@@ -86,16 +84,16 @@ app.route('/reparse').get(function(req,res){
                         'onclick': '',
                         'onmouseover': ''
                     });
-                    
+
                 });
-                
+
                 var imgs = sub('img');
-                imgs.each(function(i,v){
+                imgs.each(function(i, v) {
                     var href = imgs.eq(i).attr('src');
-                    
+
                     var url_parts = url.parse(href, true);
                     var query = url_parts.query;
-                    
+
                     console.log(href);
 
                     imgs.eq(i).attr({
@@ -103,22 +101,18 @@ app.route('/reparse').get(function(req,res){
                         'onclick': '',
                         'onmouseover': ''
                     });
-                    
+
                 });
-                
-                contents.eq(i).html('<![CDATA['+sub.html()+']]>');
+
+                contents.eq(i).html('<![CDATA[' + sub.html() + ']]>');
             });
-            
-            
+
             res.header("Content-Type", "application/rss+xml");
             res.status(200).send($.html());
-            
-      
-        
-    });
+
+        });
 });
 
-app.listen(app.get('port'), function(){
-    console.log('Listening on: ' + app.get('port'));    
+app.listen(app.get('port'), function() {
+    console.log('Listening on: ' + app.get('port'));
 });
-
